1431f259d0d66bd8c4db3808dee5d29f3d761c82
aldrinyumul9@gmail.com <aldrinyumul9@gmail.com>
2024-05-31 11:43:27 +0800

    my

--- resources/views/employee-mngt/index.blade.php
+++ resources/views/employee-mngt/index.blade.php
@@ -62,7 +62,7 @@
                       </div>
                      
                       <div class="col-12 form-group">
-                        <button class="btn btn-primary" type="submit">Submitttt</button>
+                        <button class="btn btn-primary" type="submit">darylpogi</button>
                       </div>
               </form>
 
